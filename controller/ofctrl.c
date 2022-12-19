0181b68be18e96bc4ca68a0c3e5082da34c9dcdd
张祖建 <zhangzujian.7@gmail.com>
2022-12-19 13:20:56 +0800

    set br-int controller to TCP 127.0.0.1:6653

--- controller/ofctrl.c
+++ controller/ofctrl.c
@@ -770,7 +770,7 @@ ofctrl_run(const struct ovsrec_bridge *br_int,
            const struct ovsrec_open_vswitch_table *ovs_table,
            struct shash *pending_ct_zones)
 {
-    char *target = xasprintf("unix:%s/%s.mgmt", ovs_rundir(), br_int->name);
+    char *target = strdup("tcp:127.0.0.1:6653");
     if (strcmp(target, rconn_get_target(swconn))) {
         VLOG_INFO("%s: connecting to switch", target);
         rconn_connect(swconn, target, target);
