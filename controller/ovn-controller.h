69a9701dd0276408ce00576369f4f65e05e6d59d
zhangzujian <zhangzujian.7@gmail.com>
2025-04-10 01:31:32 +0000

    add support for conditionally skipping conntrack
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- controller/ovn-controller.h
+++ controller/ovn-controller.h
@@ -18,6 +18,7 @@
 #define OVN_CONTROLLER_H 1
 
 #include <stdint.h>
+#include <stdbool.h>
 
 struct ovsrec_bridge_table;
 
@@ -25,5 +26,7 @@ const struct ovsrec_bridge *get_bridge(const struct ovsrec_bridge_table *,
                                        const char *br_name);
 
 enum chassis_tunnel_type get_tunnel_type(const char *name);
+
+extern bool lflow_kube_ovn_skip_ct;
 
 #endif /* controller/ovn-controller.h */
