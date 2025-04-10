a1e9cc03e698d618ef7a53ff7d5e14e08cff0982
zhangzujian <zhangzujian.7@gmail.com>
2025-04-10 01:29:00 +0000

    fix reaching resubmit limit in underlay
    Signed-off-by: zhangzujian <zhangzujian.7@gmail.com>

--- northd/en-global-config.c
+++ northd/en-global-config.c
@@ -721,6 +721,12 @@ check_nb_options_out_of_sync(
 
     if (config_out_of_sync(&nb->options, &config_data->nb_options,
                            "acl_ct_translation", false)) {
+                           "acl_ct_translation", false)) {
+        return true;
+    }
+
+    if (config_out_of_sync(&nb->options, &config_data->nb_options,
+                           "bcast_arp_req_flood", false)) {
         return true;
     }
 
